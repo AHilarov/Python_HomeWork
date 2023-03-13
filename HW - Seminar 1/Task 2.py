e0b303b840433bd366e032ecb1cd205db2a75022
AHilarov <ahilarov@yandex.ru>
2023-03-13 18:47:03 +0300

    егэ

--- HW - Seminar 1/Task 2.py
+++ HW - Seminar 1/Task 2.py
@@ -6,3 +6,13 @@ for x in range(2):
         for z in range(2):
             if not(x or y or z)== (not(x) and not(y) and not(z)):
                 print (x,y,z)
+
+# ((x ∧ ¬y) ≡ (z ∨ ¬w)) → (x ∧ z).
+# print('x y z w')
+# for x in range(2):
+#     for y in range(2):
+#         for z in range(2):
+#             for w in range(2):
+#                 # if not(not(not(x and not (y)) == (z or not (w))) or (x and z)):
+#                     if not(((x and not(y)) == (z or not(w))) <= (x and z)):
+#                         print (x,y,z, w)
